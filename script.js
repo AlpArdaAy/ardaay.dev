932c414d12511aa0ed4dc29ce61e4e872be1f57d
Arda Ay <73402618+AlpArdaAy@users.noreply.github.com>
2024-10-16 22:23:57 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-  // Get the menu and icon elements
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
- 
-}
